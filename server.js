7d0de27826110b318f00c23ca97061019b0148ec
karthikrahul <ramankarthik97@gmail.com>
2016-11-18 03:45:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/', function (req, res) {
 app.get('/home', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'home.html'));
 });
+app.get('/main', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
