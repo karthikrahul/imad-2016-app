0363a8f7cbeb505cd93802cf2921db32910f0752
karthikrahul <ramankarthik97@gmail.com>
2016-11-20 03:03:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,24 +5,95 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-app.get('/home', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'home.html'));
 });
-app.get('/main', function (req, res) {
+app.get('/ui/main.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.html'));
 });
-app.get('/Bill Payments', function (req, res) {
+app.get('/ui/about.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'about.html'));
+});
+app.get('/ui/Bill Payments.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Bill Payments.html'));
+});
+app.get('/ui/Channels.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Channels.html'));
+});
+app.get('/ui/Health.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Health.html'));
+});
+app.get('/ui/Indian Government Sites.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Inian Government Sites.html'));
+});
+app.get('/ui/songs.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'songs.html'));
+});
+app.get('/ui/airways.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'airways.html'));
+});
+app.get('/ui/food.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'food.html'));
+});
+app.get('/ui/online shopping.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'online shopping.html'));
+});
+app.get('/ui/railways.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'railways.html'));
+});
+app.get('/ui/sbi.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'sbi.html'));
+});
+app.get('/ui/social networking.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'social networking.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+
+app.get('/ui/links/IMG-20161113-WA0003.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'IMG-20161113-WA0003.jpg'));
+});
+app.get('/ui/links/bills.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'bills.png'));
+});
+app.get('/ui/links/food.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'food.png'));
+});
+app.get('/ui/links/health.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'health.png'));
+});
+app.get('/ui/links/i.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'i.png'));
+});
+app.get('/ui/links/indian train.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'indian train.png'));
+});
+app.get('/ui/links/li.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'li.png'));
+});
+app.get('/ui/links/online shop.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'online shop.png'));
+});
+app.get('/ui/links/plane icon.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'plane icon.jpg'));
+});
+app.get('/ui/links/social-network-icons_23-2147497527.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'social-network-icons_23-2147497527.jpg'));
+});
+app.get('/ui/links/songs icon.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'songs icon.jpg'));
+});
+app.get('/ui/links/tv icon.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'tv icon.jpg'));
+});
+app.get('/ui/links/government_iconsymbol_clip_art_thumb.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'government_iconsymbol_clip_art_thumb.jpg'));
+});
+app.get('/ui/links/sbi icon.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'sbi icon.png'));
 });
 
 
